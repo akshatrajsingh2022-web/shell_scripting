30a25252b8a9efc33ca294b3c77354fbbd02aa88
Akshat Raj Singh <akshatrajsingh2022@gmail.com>
2026-06-01 17:31:38 +0000

    shell scripting

---
+++ day01/hellow.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+#This is first script for Akshat raj
+
+echo "Akshat raj: Hello viewers"
+
+echo "Viewers: hello bhaiya,aaj hum kya karege?"
+
+echo "Akshat raj: today we wil learn abt shell scripting"
+
